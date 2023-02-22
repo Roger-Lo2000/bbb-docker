cc5a0030ef258c26d8489f4aacb1c899f5287312
RogerLo-2000 <ss37462609734@gmail.com>
2023-02-22 11:31:45 +0800

    change entrypoint.sh add npm install line

--- mod/bbb-pads/entrypoint.sh
+++ mod/bbb-pads/entrypoint.sh
@@ -9,7 +9,7 @@ jq '.etherpad.host = "10.111.83.4"' $TARGET | sponge $TARGET
 jq '.express.host = "0.0.0.0"' $TARGET | sponge $TARGET
 jq '.redis.host = "10.111.83.5"' $TARGET | sponge $TARGET
 
-
+npm install
 cd /bbb-pads
 export NODE_ENV=production
 npm start
